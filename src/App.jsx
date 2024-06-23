d18d73864b01a4f1b16e7dc469c2dd011bb387d2
Faraji Ombonya <farajiombonya@gmail.com>
2024-06-23 21:27:56 +0300

    feat: stacked form

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import StackedForm from "./components/StackedForm";
 
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <StackedForm />
     </>
   );
 }
